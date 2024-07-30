3702f2a16ab48f61bc5cae06823eb6d4ccdda0fd
Kenji <vishlano.mivali@gmail.com>
2024-07-30 21:26:03 +0400

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
   if (mongoose.connections[0].readyState) {
     return mongoose.connection.db;
   }
+
+  console.log("process.env.MONGO_URI:", process.env.MONGO_URI);
   await mongoose.connect(process.env.MONGO_URI);
   return mongoose.connection.db;
 };
