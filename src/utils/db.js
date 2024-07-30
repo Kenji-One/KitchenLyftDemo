4ebeab812dfbf3e6047a3d58acf7f2aa72f1a7e5
Kenji <vishlano.mivali@gmail.com>
2024-07-30 23:07:42 +0400

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,43 +1,13 @@
 import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// if (!MONGODB_URI) {
-//   throw new Error(
-//     "Please define the MONGODB_URI environment variable inside .env.local"
-//   );
-// }
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) {
+    return mongoose.connection.db;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose
-      .connect(process.env.MONGO_URI, opts)
-      .then((mongoose) => {
-        return mongoose;
-      });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+  console.log("process.env.MONGO_URI:", process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);
+  return mongoose.connection.db;
+};
 
 export default connectDB;
