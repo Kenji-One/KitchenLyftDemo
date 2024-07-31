8c1cb785907fea55313bc940d6b504288b6f3760
Kenji <vishlano.mivali@gmail.com>
2024-07-31 09:43:03 +0400

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,13 +1,27 @@
 import mongoose from "mongoose";
 
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGO_URI environment variable inside .env.local"
+  );
+}
+
 const connectDB = async () => {
   if (mongoose.connections[0].readyState) {
     return mongoose.connection.db;
   }
 
-  console.log("process.env.MONGO_URI:", process.env.MONGO_URI);
-  await mongoose.connect(process.env.MONGO_URI);
-  return mongoose.connection.db;
+  try {
+    console.log("Connecting to Mongo!");
+    await mongoose.connect(MONGODB_URI);
+    console.log(`MongoDB Connected`);
+    return mongoose.connection.db;
+  } catch (err) {
+    console.error("Cannot connect to the database!", err);
+    // process.exit(1);
+  }
 };
 
 export default connectDB;
