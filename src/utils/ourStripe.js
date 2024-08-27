6d88136df7fa29a2791be839eea0969ffa038c25
Kenji <vishlano.mivali@gmail.com>
2024-08-27 23:26:33 +0400

    Update ourStripe.js

--- src/utils/ourStripe.js
+++ src/utils/ourStripe.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
-
+console.log("Stripe Secret Key:", process.env.STRIPE_SECRET_KEY);
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
